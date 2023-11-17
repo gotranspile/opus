a8d823458eb4eba28deb8c51e45afccc7855b6c0
Denys Smirnov <dennwc@pm.me>
2023-11-17 19:25:49 +0200

    silk: Move DecodeParameters.

--- libopus/silk_decode_pitch.go
+++
@@ -1,9 +0,0 @@
-package libopus
-
-import (
-	"github.com/gotranspile/opus/silk"
-)
-
-func silk_decode_pitch(lagIndex int16, contourIndex int8, pitch_lags []int, Fs_kHz int, nb_subfr int) {
-	silk.DecodePitch(lagIndex, contourIndex, pitch_lags, Fs_kHz, nb_subfr)
-}
